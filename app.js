db2fe2b5cfad1993f90a568363c3069410d1175e
David Howard <hotcheesesoup6@aol.com>
2022-02-14 14:39:36 -0600

    Objects and Arrays exercise

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-console.log("Hello World!\n==========\n");
+// Exercise 1
+/** Takes an array of numbers as a parameter and returns the sum value of each number in the array. */
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const numbers = [2, 22, 12, 17, 18, 39, 129];
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+function arraySum(arr) {
+  let sum = 0;
 
+  arr.forEach(function (num) {
+    sum += num;
+  });
 
+  return sum;
+}
+
+let result = arraySum(numbers);
+
+console.log(result);
+
+//Exercise 2
+
+const book = {};
+
+book.title = prompt("What is the title of your favorite book?");
+book.author = prompt("What is the author of your favorite book?");
+book.pageCount = prompt("How many pages does your favorite book have?");
+book.readCount = prompt("How many times have you read your favorite book?");
+
+book.info = function () {
+    return `${this.title} by ${this.author} is ${this.pageCount} page(s) long, that has been read ${this.readCount} 
+  times(s).`;
+};
+
+console.log(book.info());
